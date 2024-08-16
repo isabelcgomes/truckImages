0e9e148448ed786aea106b0ac2e8aa560699b94f
isabelcgomes <isabel.gomes2013@outlook.com>
2024-08-16 14:17:59 -0300

    script organize directories

--- move.py
+++ move.py
@@ -3,5 +3,5 @@ import os
 categories = [str(cat[0:-4]) for cat in os.listdir("CAD_Files\stl")]
 
 for cat in categories:
-    for file in os.listdir(fr"images\zoom\{cat}"):
-        os.rename(fr"images\zoom\{cat}\{file}", fr"images\{cat}\{file}")
+    for file in os.listdir(fr"images\bg\{cat}"):
+        os.rename(fr"images\bg\{cat}\{file}", fr"images\{cat}\{file}")
