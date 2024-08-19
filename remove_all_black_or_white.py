7205416e3478f7beeaf49fbde87ccb3d4711e38e
isabelcgomes <isabel.gomes2013@outlook.com>
2024-08-18 22:13:16 -0300

    script remove generalization (function creation)

--- remove_all_black_or_white.py
+++ remove_all_black_or_white.py
@@ -5,6 +5,13 @@ import os
 
 categories = [str(cat[0:-4]) for cat in os.listdir("CAD_Files\stl")]
 
+def remove_images(path, cat, image):
+    im = Image.open(fr'{path}\{cat}\{image}') 
+    extrema = im.convert("L").getextrema()
+    if extrema != (0, 255):
+        os.remove(fr'{path}\{cat}\{image}')
+
+
 for cat in categories:
     for image in os.listdir(fr'images\zoom\{cat}'):
         im = Image.open(fr'images\zoom\{cat}\{image}') 
