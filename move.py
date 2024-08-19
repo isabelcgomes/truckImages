a400e2acdbe6d34dee8a822bdfad5f72cf509980
isabelcgomes <isabel.gomes2013@outlook.com>
2024-08-18 23:10:41 -0300

    script move images

--- move.py
+++ move.py
@@ -3,5 +3,10 @@ import os
 categories = [str(cat[0:-4]) for cat in os.listdir("CAD_Files\stl")]
 
 for cat in categories:
-    for file in os.listdir(fr"images\bg\{cat}"):
-        os.rename(fr"images\bg\{cat}\{file}", fr"images\{cat}\{file}")
+    try:
+        for file in os.listdir(fr'overimages\bg\{cat}'):
+                os.rename(fr"overimages\bg\{cat}\{file}", fr"overimages\{cat}\{file}")
+        for file in os.listdir(fr'overimages\{cat}'):
+            os.rename(fr"overimages\{cat}\{file}", fr"images\{cat}\{file}")
+    except:
+         next
